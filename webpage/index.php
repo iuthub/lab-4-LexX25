100e7b68348c697c5d691a68007ae0ecfa1184cb
Lex25-zxc <leshamelnik00@gmail.com>
2021-03-14 21:08:12 +0500

    commit1

---
+++ webpage/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
+    "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <title>Music Viewer</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+    <link href="viewer.css" type="text/css" rel="stylesheet" />
+</head>
+<body>
+<div id="header">
+
+    <h1>190M Music Playlist Viewer</h1>
+    <h2>Search Through Your Playlists and Music</h2>
+</div>
+<div id="listarea">
+    <ul id="musiclist">
+        <?php
+        $songs=glob("songs/*.mp3");
+        foreach ($songs as $song ){
+        ?>
+        <li class="mp3item">
+            <a href="<?=$song?>">
+                <?=basename($song)?>
+            </a>
+        </li>
+        <?php }?>
+        <?php
+        $items=glob("songs/*.txt");
+        foreach($items as $item){
+        ?>
+    <li class="playlistitem">
+        <a href="<?=$item?>">
+            <?=basename($item)?>
+        </a>
+    </li>
+        <?php } ?>
+    </ul>
+
+</div>
+</body>
+</html>
+
